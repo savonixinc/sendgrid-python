1f69fe0b1bb4b36e415b19da1470b5808a81f0ae
Konstantin Vinogradov <kostya.vinogradov@gmail.com>
2020-04-11 14:18:49 +0600

    Reworket internal/external client session logic

--- sendgrid/async_sendgrid.py
+++ sendgrid/async_sendgrid.py
@@ -3,6 +3,7 @@ import sys
 if sys.version_info < (3, 5):
     raise ImportError("Python 3.5 or later is required")
 
+from typing import Optional, Union
 import aiohttp
 import python_http_client
 
@@ -34,32 +35,40 @@ class AsyncSendGridAPIClient(SendGridAPIClient):
         asyncio.run(send_message())
     """
 
-    def __init__(self, *args, client_session=None, **kwargs):
-        self._client_session = client_session or aiohttp.ClientSession()
-        self.use_external_session = client_session is not None
+    def __init__(
+        self,
+        *args,
+        client_session:Optional[aiohttp.ClientSession]=None,
+        **kwargs):
+
+        self._client_session = None
         super().__init__(*args, **kwargs)
+        if client_session:
+            self.set_client_session(client_session)
 
     async def __aenter__(self):
+        if not self.get_client_session():
+            self.set_client_session(aiohttp.ClientSession(), True)
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        if not self.use_external_session:
-            await self.close_client_session()
-
-    @property
-    def client_session(self):
+        if self.internal_client_session_used:
+            await self.get_client_session().close()
+    
+    def get_client_session(self) -> Union[aiohttp.ClientSession, None]:
         return self._client_session
-
-    def get_client(self):
+    
+    def set_client_session(self, session: aiohttp.ClientSession, internal: bool = False):
+        self.client.set_client_session(session)
+        self._client_session = session
+        self.internal_client_session_used = internal
+        
+    def get_client(self) -> python_http_client.AsyncClient:
         return python_http_client.AsyncClient(
             host=self.host,
-            client_session=self.client_session,
+            client_session=self.get_client_session(),
             request_headers=self._default_headers,
             version=3)
-
-    async def close_client_session(self):
-        if not self.use_external_session:
-            await self._client_session.close()
 
     async def send(self, message):
         return await super().send(message)
